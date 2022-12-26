2943e65005973a16a249f17d10ac40a51940352e
Alex Kalkatos <alexcsts@gmail.com>
2022-12-26 00:51:36 -0300

    First commit

---
+++ Specific/FunctionsGameModels/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Kalkatos.FunctionsGame.Models
+{
+	public class LoginRequest
+	{
+		public string DeviceId;
+	}
+
+	public class LoginResponse
+	{
+		public bool IsNewUser;
+		public string SessionKey;
+	}
+}
